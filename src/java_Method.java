bcd0265ead804372bd5aa9e7a380051008f1477d
Peason <peasonthepea@gmail.com>
2026-06-04 15:31:07 -0400

    Create Java_Method

---
+++ src/java_Method.java
@@ -0,0 +1,7 @@
+// A method is a block of code which only runs when it is called.
+// They are also known as functions (Which is the same name in python)
+public class java_Method {
+    static void myMethod(){
+        
+    }
+}
